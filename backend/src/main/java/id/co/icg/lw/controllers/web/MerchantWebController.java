bcf7be99dbe2321b4b697c74135e781297d85fb0
alifkurniawan <cs.alif.kurniawan@gmail.com>
2018-01-24 08:40:42 +0700

    penambahan table : merchant, merchant category, user

---
+++ backend/src/main/java/id/co/icg/lw/controllers/web/MerchantWebController.java
@@ -0,0 +1,40 @@
+package id.co.icg.lw.controllers.web;
+
+import id.co.icg.lw.domain.merchant.Merchant;
+import id.co.icg.lw.domain.merchant.MerchantCategory;
+import id.co.icg.lw.services.merchant.MerchantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/merchant")
+public class MerchantWebController {
+
+    @Autowired
+    private MerchantService merchantService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String getMerchant(Model model) {
+        List<Merchant> merchants = merchantService.findAllMerchant();
+        model.addAttribute("merchants", merchants);
+        return "merchant/index";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String getMerchantForm(Model model) {
+        return "merchant/create_merchant";
+    }
+
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
+    public String getMerchantCategory(Model model) {
+        List<MerchantCategory> categories = merchantService.findAllMerchantCategory();
+        model.addAttribute("categories", categories);
+        return "merchant/category";
+    }
+}
--- backend/src/main/java/id/co/icg/lw/controllers/web/UserController.java
+++ backend/src/main/java/id/co/icg/lw/controllers/web/UserController.java
@@ -1,17 +1,29 @@
 package id.co.icg.lw.controllers.web;
 
+import id.co.icg.lw.domain.user.User;
+import id.co.icg.lw.services.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public void getUser(@RequestParam("page") int page, Model model) {
+    public String getUser(@RequestParam(name = "page", defaultValue = "1") int page, Model model) {
+        List<User> users = userService.findAll();
+
+        model.addAttribute("users", users);
+        return "/user/index";
 
     }
 
