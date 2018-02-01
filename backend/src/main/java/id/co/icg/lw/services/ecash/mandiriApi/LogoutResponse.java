7209779c375cf9311169da2351d5c02700af7e20
alifkurniawan <cs.alif.kurniawan@gmail.com>
2018-02-01 14:44:58 +0700

    penambahan e cash

---
+++ backend/src/main/java/id/co/icg/lw/services/ecash/mandiriApi/LogoutResponse.java
@@ -0,0 +1,13 @@
+package id.co.icg.lw.services.ecash.mandiriApi;
+
+public class LogoutResponse {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
