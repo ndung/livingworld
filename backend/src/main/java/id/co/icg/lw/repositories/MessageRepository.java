f707e05553ad964a2d22e686e7b8cb6c46f6c84c
alifkurniawan <cs.alif.kurniawan@gmail.com>
2018-02-03 14:38:40 +0700

    order by create at message

--- backend/src/main/java/id/co/icg/lw/repositories/MessageRepository.java
+++ backend/src/main/java/id/co/icg/lw/repositories/MessageRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Transactional
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    @Query("select new id.co.icg.lw.services.message.MessageResponse(m) from Message m where m.receiver is null or m.receiver = :receiver")
+    @Query("select new id.co.icg.lw.services.message.MessageResponse(m) from Message m where m.receiver is null or m.receiver = :receiver order by m.createAt desc ")
     List<MessageResponse> findAllByReceiverOrderByCreateAtDesc(@Param("receiver") User receiver, Pageable pageable);
 
     @Query("select new id.co.icg.lw.services.message.MessageResponse(m) from Message m where m.receiver is null order by m.createAt desc ")
