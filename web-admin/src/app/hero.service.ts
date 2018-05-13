671b2c975d86be8b0dde4be8f8da9ab79ada062b
alifkurniawan <cs.alif.kurniawan@gmail.com>
2018-05-13 10:23:28 +0700

    detail

--- web-admin/src/app/hero.service.ts
+++ web-admin/src/app/hero.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
+import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetch heroes');
+    return of(HEROES);
   }
 
 }
